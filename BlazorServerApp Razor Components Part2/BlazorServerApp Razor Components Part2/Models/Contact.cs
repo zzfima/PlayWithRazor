41570b0071f94fa4a16effc44c59a7e7e89570bf
Efim Zabarsky <[Efim.Zabarsky@kla-tencor.com]>
2024-06-29 16:48:22 +0300

    add callback function

--- BlazorServerApp Razor Components Part2/BlazorServerApp Razor Components Part2/Models/Contact.cs
+++ BlazorServerApp Razor Components Part2/BlazorServerApp Razor Components Part2/Models/Contact.cs
@@ -3,8 +3,8 @@
     [Serializable]
     public class Contact
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
     }
 }
