b3e7b0bce34ac63aec7b1e4c6b2a03fa5c8d43dc
zzfima <zzfima@gmail.com>
2024-06-29 09:30:15 +0300

    start

---
+++ BlazorServerApp Razor Components Part2/BlazorServerApp Razor Components Part2/Docs/README.md
@@ -0,0 +1,3 @@
+From here: https://www.youtube.com/watch?v=Sp2XYGqNeD8&list=PLzewa6pjbr3IQEUfNiK2SROQC1NuKl6PV
+
+![](1.png "what is razor components?")
---
+++ BlazorServerApp Razor Components Part2/BlazorServerApp Razor Components Part2/Models/Contact.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorServerApp_Razor_Components_Part2.Models
+{
+    [Serializable]
+    public class Contact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
